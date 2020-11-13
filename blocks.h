1fcea6e36d53d1f7f5800d3319fb313a337beb48
a_xyl <by.masterm@gmail.com>
2020-11-14 02:58:11 +1100

    Migrated functions to shell scripts

--- blocks.h
+++ blocks.h
@@ -7,8 +7,8 @@ static const Block blocks[] = {
 	{"",		"spotifytrack.sh",	1,			0},
 	{"/dev/sda3 usage: ", "df -h | grep \"/dev/sda3\" | awk '{print $3\"/\"$2\", \"$5}'",	1,		0},
 	{"Mem: ", 	"memoryusage.sh",	1,			0},
-	{"CPU: ", "echo $[100-$(vmstat 1 2|tail -1|awk '{print $15}')]%",	1,		0},
-	{"", 		"date '+%b %d (%a) %I:%M%p'",		60,	0},
+	{"CPU: ", 	"cpuusage.sh",		1,			0},
+	{"Date: ",	"datetime.sh",		60,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
--- programs/memoryusage.sh
+++ programs/memoryusage.sh
@@ -1,9 +1,1 @@
-#!/bin/sh
-
-MemUsage=$(free -h | awk '/^Mem/ { print $3"/"$2 }' | sed s/i//g)
-MemUsagePercentage=$(free -h | awk '/^Mem/ { print $3/$2*100 }' | sed s/Gi//g)
-
-MemUsageRounded=$(printf "%.0f" $MemUsagePercentage)
-
-printf $MemUsage", "$MemUsageRounded"%%\n"
-
+free --mebi | sed -n '2{p;q}' | awk '{printf ("%2.2fGB/%2.2fGB, %2.2f%%\n", ( $3 / 1024 ), ($2 / 1024), ($3/$2*100))}'

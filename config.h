53429ad91ef33af0e3571447b20392049eafaa54
a_xyl <by.masterm@gmail.com>
2020-11-30 21:59:30 +1100

    Replaced dwmblocks build with LukeSmith dwmblocks

--- config.h
+++ config.h
@@ -1,36 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "kbselect", 0, 30}, */
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
-	/* {"",	"crypto",	0,	13}, */
-	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
-	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
-	/* {"",	"price link \"Chainlink\" 🔗",			300,	25}, */
-	/* {"",	"price xmr \"Monero\" 🔒",			9000,	24}, */
-	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
-	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
-	/* {"",	"torrent",	20,	7}, */
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	18}, */
-	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"forecast",	18000,	5},
-	{"",	"mailbox",	180,	12},
-	{"",	"nettraf",	1,	16},
-	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	3},
-	{"",	"clock",	60,	1},
-	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+	{"Volume: ", 	"alsa.sh",		1,			0},
+	{"Weather: ",	"currentweather.sh",	3600,			0},
+	{"",		"spotifytrack.sh",	1,			0},
+	{"/dev/sda3: 💽 ", "diskusage.sh",	5,			0},
+	{"Mem:  ", 	"memoryusage.sh",	1,			0},
+	{"CPU:  ", 	"cpuusage.sh",		1,			0},
+	{"Date:  ",	"datetime.sh",		60,			0},
 };
 
-//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
-
-// Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char *delim = " | ";
+static unsigned int delimLen = 5;
